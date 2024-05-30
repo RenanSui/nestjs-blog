f75f9a65992e3c487db25be43b3f399c6c1e8db2
RenanSui <renanddtao@gmail.com>
2024-05-30 15:17:39 -0300

    remove comments and unnused files

--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -1,17 +1,5 @@
 import { Prisma } from '@prisma/client'
 
 export class LoginResponseDto {
-  // @ApiResponseProperty()
-  // token: string
-
-  // @ApiResponseProperty()
-  // refreshToken: string
-
-  // @ApiResponseProperty()
-  // tokenExpires: number
-
-  // @ApiResponseProperty({
-  //   type: () => Prisma.UserCreateInput,
-  // })
   user: Prisma.UserCreateInput
 }
