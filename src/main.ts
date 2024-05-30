20f2bedbdc2d7cc30aa43b385373d9106a4712da
RenanSui <renanddtao@gmail.com>
2024-05-30 15:10:07 -0300

    add configurations to app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.enableCors()
+  app.use(cookieParser())
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(8000)
 }
 bootstrap()
