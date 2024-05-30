0a869f26c26071cd3b5722cafb2397f1ae3ffbe9
RenanSui <renanddtao@gmail.com>
2024-05-30 15:11:38 -0300

    create request types

---
+++ src/types/request.ts
@@ -0,0 +1,11 @@
+import { Prisma } from '@prisma/client'
+import { Request } from 'express'
+
+export interface ContextRequest<T> extends Omit<Request, 'context'> {
+  context: T
+}
+
+export interface UserRequest {
+  user: Prisma.UserCreateInput
+  accessToken: string
+}
