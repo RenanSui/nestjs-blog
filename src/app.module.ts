486bbc5597f88f3d278f7b25aede389cfc8515c3
RenanSui <renanddtao@gmail.com>
2024-05-31 14:46:54 -0300

    extend app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { DatabaseModule } from './database/database.module'
+import { ProfileModule } from './profile/profile.module'
 import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [AuthModule, UserModule, DatabaseModule],
+  imports: [AuthModule, UserModule, DatabaseModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
