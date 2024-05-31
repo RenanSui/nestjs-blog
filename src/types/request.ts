5b19d7b2ab6a4f0df484a9dfd40b68d8189f4a62
RenanSui <renanddtao@gmail.com>
2024-05-31 14:47:07 -0300

    extend request type

--- src/types/request.ts
+++ src/types/request.ts
@@ -9,3 +9,8 @@ export interface UserRequest {
   user: Prisma.UserCreateInput
   accessToken: string
 }
+
+export interface ProfileRequest {
+  profile: Prisma.ProfileCreateInput
+  accessToken: string
+}
