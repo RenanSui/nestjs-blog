6862d399d672556efc841f98d729c6ace441a3b1
RenanSui <renanddtao@gmail.com>
2024-06-06 08:37:41 -0300

    create post resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module'
 import { DatabaseModule } from './database/database.module'
 import { ProfileModule } from './profile/profile.module'
 import { UserModule } from './user/user.module'
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule, UserModule, DatabaseModule, ProfileModule],
+  imports: [AuthModule, UserModule, DatabaseModule, ProfileModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
