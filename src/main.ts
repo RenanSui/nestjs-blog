6a207050b20d01eda78c913c92867f9ed770930c
RenanSui <renanddtao@gmail.com>
2024-06-13 14:12:14 -0300

    modify -> cors: options

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
+import { StatusCodes } from 'http-status-codes'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
+  const configService = app.get(ConfigService)
+
+  app.enableCors({
+    origin: configService.get<string>('CLIENT_URL'),
+    credentials: true,
+    optionsSuccessStatus: StatusCodes.OK,
+  })
   app.use(cookieParser())
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
+
   await app.listen(8000)
 }
 bootstrap()
