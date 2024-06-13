620e011dbea8c6f2168c0947910e72cb1b521a64
RenanSui <renanddtao@gmail.com>
2024-06-13 14:11:18 -0300

    add -> configModule on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
+import { config } from './config/config'
 import { DatabaseModule } from './database/database.module'
+import { PostModule } from './post/post.module'
 import { ProfileModule } from './profile/profile.module'
 import { UserModule } from './user/user.module'
-import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [AuthModule, UserModule, DatabaseModule, ProfileModule, PostModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    DatabaseModule,
+    ProfileModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
