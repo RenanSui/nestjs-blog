8a2aea0d7d2f6bbe7c49b9a1c8fd603dfcbac788
RenanSui <renanddtao@gmail.com>
2024-06-20 19:26:37 -0300

    add configService again

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { StatusCodes } from 'http-status-codes'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: process.env.CLIENT_URL,
-      credentials: true,
-      methods: ['GET', 'POST', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      optionsSuccessStatus: StatusCodes.OK,
-    },
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: StatusCodes.OK,
   })
 
   app.use(cookieParser())
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(process.env.SERVER_PORT)
+  await app.listen(configService.get<string>('SERVER_PORT'))
 }
 bootstrap()
